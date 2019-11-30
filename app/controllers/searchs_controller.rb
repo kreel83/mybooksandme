870378bbf1e5583967534d3a6b7cd40b6a19a9e7
Marc BORGNA <marc.borgna@gmail.com>
2019-12-01 00:53:07 +0100

    search part 1

---
+++ app/controllers/searchs_controller.rb
@@ -0,0 +1,15 @@
+require 'json'
+require 'open-uri'
+
+class SearchsController < ApplicationController
+  def index
+      query = params[:search]
+      query = 'les+miserables' if !params[:search]
+
+      url = "https://www.googleapis.com/books/v1/volumes?q=#{query}&projection=lite&langRestrict=fr"
+      f = open(url).read
+      fj = JSON.parse(f)
+      @fj = fj['items']
+
+  end
+end
